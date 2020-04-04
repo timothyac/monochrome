4bb8cb419d84153198a05f47ba3e38072641fdd8
sunstro <sunstrous@gmail.com>
2020-04-04 17:13:25 -0400

    changing hover styling on nav links

--- src/components/nav/NavLink.js
+++ src/components/nav/NavLink.js
@@ -1,32 +1,27 @@
 import styled from "styled-components";
 
 export default styled.li.attrs(({ active }) => ({
-  border: active ? "#fff" : "#333",
+  activeColor: active ? "#fff" : "#333",
 }))`
   cursor: pointer;
   position: relative;
   text-align: center;
   min-width: 100px;
   text-transform: capitalize;
-  color: #fff;
+  color: ${(props) => props.activeColor};
+  transition: color 0.2s ease-in;
 
   &:after {
     content: " ";
     height: 30px;
     width: 100%;
-    border-bottom: 1px solid ${(props) => props.border};
+    border-bottom: 1px solid ${(props) => props.activeColor};
     position: absolute;
     left: 0;
     top: 13px;
   }
 
-  &:hover::after {
-    content: " ";
-    height: 30px;
-    width: 100%;
-    border-bottom: 1px solid #fff;
-    position: absolute;
-    left: 0;
-    top: 13px;
+  &:hover {
+    color: #fff;
   }
 `;
