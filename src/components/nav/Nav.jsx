105d79b910d4de3aa1b3763d85bb9bbcc7dd6af8
sunstro <sunstrous@gmail.com>
2020-04-04 17:03:13 -0400

    nav uses links now

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import { useLocation, Link } from "react-router-dom";
 
 import { nav, inner } from "./Nav.module.scss";
 import NavLink from "./NavLink";
 
+const pages = [
+  { name: "dashboard", link: "/" },
+  { name: "tasks" },
+  { name: "proxies" },
+  { name: "accounts" },
+];
+
 export default () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className={nav}>
       <div className={inner}>
         <h2>monochrome</h2>
         <ul>
-          <NavLink {...{ active: true }}>dashboard</NavLink>
-          <NavLink>tasks</NavLink>
-          <NavLink>proxies</NavLink>
-          <NavLink>accounts</NavLink>
+          {pages.map((page, i) => {
+            const pageLink = page.link ? `${page.link}` : `/${page.name}`;
+            const active = pathname === pageLink;
+
+            return (
+              <Link key={i} to={pageLink}>
+                <NavLink {...{ active }}>{page.name}</NavLink>
+              </Link>
+            );
+          })}
         </ul>
         <div>
           <p>sunstro</p>
