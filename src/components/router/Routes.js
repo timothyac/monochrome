44f70a1c978f8bdda0293d6ad0329695c375ef8d
sunstro <sunstrous@gmail.com>
2020-04-04 16:46:16 -0400

    routes needs path prop

--- src/components/router/Routes.js
+++ src/components/router/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch } from "react-router-dom";
 
 import Dashboard from "../dashboard/Dashboard";
+import Tasks from "../tasks/Tasks";
 import Route from "./Route";
 
 export default () => {
   return (
     <Switch>
-      <Route component={Dashboard} />
+      <Route path="/tasks" component={Tasks} />
+      <Route path="/" component={Dashboard} />
     </Switch>
   );
 };
