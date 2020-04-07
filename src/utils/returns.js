6fbb2cc5dff78a3a6e0c9b7f99e3898aeef38eac
sunstro <sunstrous@gmail.com>
2020-04-07 02:10:23 -0400

    Moved task logic into returns lib

--- src/utils/returns.js
+++ src/utils/returns.js
@@ -2,3 +2,10 @@ export const returnColor = (status) => {
   if (status === "online") return "#48bb78";
   if (status === "offline") return "#F56565";
 };
+
+export const returnTaskStatus = (status) => {
+  if (status === "Success") return ["#48bb78", "Upload"];
+  if (status === "Failure") return ["#F56565", "Restart"];
+  if (status === "Ready") return ["#4299E1", "Start"];
+  if (status === "Running") return ["#ECC94B", "Stop"];
+};
