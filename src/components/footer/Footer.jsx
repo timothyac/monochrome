9b380cb18a0ef2867d8898d1bde2a53c9173508c
sunstro <sunstrous@gmail.com>
2020-04-07 02:05:27 -0400

    Footer now shows server status

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import { footer } from "./Footer.module.scss";
+import Status from "./Status";
 
 export default () => {
+  // Should be replace by an API call
+  const status = "online";
+
   return (
     <footer className={footer}>
-      <h4>footer</h4>
+      <Status {...{ status }} />
     </footer>
   );
 };
