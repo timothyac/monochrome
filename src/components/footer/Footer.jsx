06c00c52d9efc9cf7919065893ed7af9260f5dfc
sunstro <sunstrous@gmail.com>
2020-04-07 02:28:28 -0400

    Now displaying copyright and version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-import { footer } from "./Footer.module.scss";
+import { footer, copyright } from "./Footer.module.scss";
 import Status from "./Status";
 
 export default () => {
-  // Should be replace by an API call
+  // API call to server
   const status = "online";
 
   return (
     <footer className={footer}>
+      <div className={copyright}>
+        <p>
+          copyright ® 2020 <span>monochrome</span>
+        </p>
+      </div>
+      <div>
+        <p>version 0.1.0.0</p>
+      </div>
       <Status {...{ status }} />
     </footer>
   );
