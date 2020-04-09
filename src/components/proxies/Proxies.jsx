b350288ab65a8295ddd3bb5469587a59d34c9587
sunstro <sunstrous@gmail.com>
2020-04-09 13:14:29 -0400

    starting purchase form

--- src/components/proxies/Proxies.jsx
+++ src/components/proxies/Proxies.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { page, purchase } from "./Proxies.module.scss";
+
 export default () => {
   return (
-    <div>
-      <h1>Proxies</h1>
+    <div className={page}>
+      <div className={purchase}>
+        <h1>Purchase</h1>
+        <form>
+          <select name="providers" id="providers">
+            <option value="Ashburn">Ashburn</option>
+          </select>
+        </form>
+      </div>
+      <div>
+        <h1>Proxies</h1>
+      </div>
     </div>
   );
 };
