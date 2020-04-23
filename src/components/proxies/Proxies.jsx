2aed8f4a76548be4ee2c553c4603686f0803dc99
sunstro <sunstrous@gmail.com>
2020-04-22 20:05:30 -0400

    added styled component to proxies and removed extra styling

--- src/components/proxies/Proxies.jsx
+++ src/components/proxies/Proxies.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { page, purchase } from "./Proxies.module.scss";
-import { Button } from "../tasks/Button";
+import { Form } from "./Form";
 
 export default () => {
   const purchaseProxies = (e) => {
@@ -17,31 +17,7 @@ export default () => {
     <div className={page}>
       <div className={purchase}>
         <h1>Purchase</h1>
-        <form onSubmit={purchaseProxies}>
-          <div>
-            <p>Location</p>
-            <select name="providers" id="providers">
-              <option value="Ashburn">Ashburn</option>
-              <option value="Boston">Boston</option>
-              <option value="Chicago">Chicago</option>
-              <option value="New York">New York</option>
-            </select>
-          </div>
-          <div>
-            <p>Amount (Max. 99)</p>
-            <input type="tel" maxLength="2" />
-          </div>
-          <div>
-            <p>Profile</p>
-            <select name="profiles" id="profiles">
-              <option value="AMEX 1">AMEX 1</option>
-              <option value="Dad 1">Dad 2</option>
-              <option value="Dad 2">Dad 2</option>
-              <option value="Privacy">Privacy</option>
-            </select>
-          </div>
-          <Button>Purchase</Button>
-        </form>
+        <Form {...{ purchaseProxies }} />
       </div>
       <div>
         <h1>Proxies</h1>
